1553f56bc629fc634c7b16e487beee9bdc3797cd
Jhordan <jhordan.laureano@unmsm.edu.pe>
2024-07-09 23:38:12 -0500

    Agregando vehiculo 2

--- run_TSP.py
+++ run_TSP.py
@@ -14,13 +14,12 @@ if __name__ == '__main__':
         print ("driver", driver)
         MR = GA.GenerateMatrix(path+str(driver)+"_distance.txt")
         if len(MR) < 100:
-            # Aumentar tamaño de la población, tasa de mutación y número de generaciones
-            results.append(GA.main(MR, 150, 15, 15, 0.7, 3000, 3000))
-            MS = GA.GenerateMatrix(path + str(driver) + "_matrix_with_speed.txt")
-            results.append(GA.main(MS, 150, 15, 15, 0.7, 3000, 3000))
-            MG = GA.GenerateMatrix(path + str(driver) + "_matrix_with_load_gradient.txt")
-            results.append(GA.main(MG, 150, 15, 15, 0.7, 3000, 3000))
-            ME = GA.GenerateMatrix(path + str(driver) + "_EU_2020.txt")
+            results.append(GA.main(MR,30,3,3,0.1,1000,1000))
+            MS = GA.GenerateMatrix(path+str(driver)+"_matrix_with_speed.txt")
+            results.append(GA.main(MS,30,3,3,0.1,1000,1000))
+            MG = GA.GenerateMatrix(path+str(driver)+"_matrix_with_load_gradient.txt")
+            results.append(GA.main(MG,30,3,3,0.1,1000,1000))
+            ME =GA.GenerateMatrix(path+str(driver)+"_EU_2020.txt")
         print (results)
         table = []
         for sol in results:
